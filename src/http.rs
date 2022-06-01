2faf91fe32e72ca5063db649e0fddae11d2b3480
Marcus Rådell <marcus@radell.net>
2022-06-01 10:14:51 +0200

    use TracingLogger instead of Logger (also formatted file)

--- src/http.rs
+++ src/http.rs
@@ -1,30 +1,42 @@
-use actix_cors::Cors;
-use actix_web::{http, middleware::Logger, dev::{ServiceFactory, ServiceRequest, ServiceResponse}, body::MessageBody, Error, web};
+use crate::ensure_env;
 use crate::health;
+use actix_cors::Cors;
+use actix_web::{
+    body::MessageBody,
+    dev::{ServiceFactory, ServiceRequest, ServiceResponse},
+    http, web, Error,
+};
+use tracing_actix_web::TracingLogger;
 
-use super::ensure_env;
-
-pub fn new(configs: Vec<fn(&mut web::ServiceConfig)>) -> actix_web::App<
+pub fn new(
+    configs: Vec<fn(&mut web::ServiceConfig)>,
+) -> actix_web::App<
     impl ServiceFactory<
         ServiceRequest,
         Config = (),
         Response = ServiceResponse<impl MessageBody>,
         InitError = (),
-        Error = Error>> {
+        Error = Error,
+    >,
+> {
     let frontend_host = ensure_env("FRONTEND_HOST");
 
     let cors = Cors::default()
-            .allowed_origin(&frontend_host)
-            .allowed_methods(vec!["GET", "POST"])
-            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT, http::header::CONTENT_TYPE])
-            .max_age(3600);
-                    
+        .allowed_origin(&frontend_host)
+        .allowed_methods(vec!["GET", "POST"])
+        .allowed_headers(vec![
+            http::header::AUTHORIZATION,
+            http::header::ACCEPT,
+            http::header::CONTENT_TYPE,
+        ])
+        .max_age(3600);
+
     let app = actix_web::App::new()
-        .wrap(Logger::default())
+        .wrap(TracingLogger::default())
         .wrap(cors);
 
     let mut scope = web::scope("");
-    
+
     for config in configs.clone() {
         scope = scope.configure(config);
     }

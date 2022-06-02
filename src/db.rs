19ad72326a4dd87e46613714896fc6279edf67c1
emurman <emilmurman@hotmail.com>
2022-06-02 11:58:11 +0200

    add postgres init code

--- src/db.rs
+++ src/db.rs
@@ -1,21 +1,21 @@
 use std::path::Path;
 
 use crate::ensure_env;
-use sqlx::{mysql::MySqlPoolOptions, MySql, Pool};
+use sqlx::{mysql::MySqlPoolOptions, MySql, Pool, Postgres, postgres::PgPoolOptions};
 
-pub async fn init_mysql() -> Pool<MySql> {
+
+fn init() -> (String, bool) {
     tracing::info!("DB initializing...");
 
     let db_uri = ensure_env("DATABASE_URL");
     let migrate_db = ensure_env("MIGRATE_DB") == "ON";
 
-    let pool = MySqlPoolOptions::new()
-        .connect(&db_uri)
-        .await
-        .expect("Failed to connect to DB.");
+    (db_uri, migrate_db)
+}
 
-    let _row: (i64,) = sqlx::query_as("SELECT 1")
-        .fetch_one(&pool)
+async fn finish_pg(pool: &Pool<Postgres>, migrate_db: bool) {
+    let _row: (i32,) = sqlx::query_as("SELECT 1")
+        .fetch_one(pool)
         .await
         .expect("Failed while ensuring DB connection.");
 
@@ -26,7 +26,7 @@ pub async fn init_mysql() -> Pool<MySql> {
         sqlx::migrate::Migrator::new(Path::new("./migrations"))
             .await
             .expect("Migrator could not be created.")
-            .run(&pool)
+            .run(pool)
             .await
             .map_err(|e| {
                 tracing::error!("{}", e);
@@ -38,6 +38,58 @@ pub async fn init_mysql() -> Pool<MySql> {
         tracing::info!("Migrations skipped.");
     }
 
-    tracing::info!("DB initialized!");
+    tracing::info!("DB initialized!");    
+}
+
+pub async fn init_pg() -> Pool<Postgres> {
+    let (db_uri, migrate_db) = init();
+
+    let pool = PgPoolOptions::new()
+        .connect(&db_uri)
+        .await
+        .unwrap();
+
+    finish_pg(&pool, migrate_db).await;
+
+    pool
+}
+
+async fn finish_mysql(pool: &Pool<MySql>, migrate_db: bool) {
+    let _row: (i64,) = sqlx::query_as("SELECT 1")
+        .fetch_one(pool)
+        .await
+        .expect("Failed while ensuring DB connection.");
+
+    tracing::info!("DB connection ensured.");
+
+    if migrate_db {
+        tracing::info!("Migrations started...");
+        sqlx::migrate::Migrator::new(Path::new("./migrations"))
+            .await
+            .expect("Migrator could not be created.")
+            .run(pool)
+            .await
+            .map_err(|e| {
+                tracing::error!("{}", e);
+                e
+            })
+            .expect("Failed to run migrations!");
+        tracing::info!("Migrated DB!");
+    } else {
+        tracing::info!("Migrations skipped.");
+    }
+
+    tracing::info!("DB initialized!");    
+}
+
+pub async fn init_mysql() -> Pool<MySql> {
+    let (db_uri, migrate_db) = init();
+
+    let pool = MySqlPoolOptions::new()
+        .connect(&db_uri)
+        .await
+        .expect("Failed to connect to DB.");
+
+    finish_mysql(&pool, migrate_db).await;
     pool
 }

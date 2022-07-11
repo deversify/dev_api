bf928f6c2d676197a4fae928c155b935f942efc9
Marcus Rådell <marcus@radell.net>
2022-07-11 14:47:49 +0200

    add types for hash and verify

--- src/password.rs
+++ src/password.rs
@@ -14,12 +14,16 @@ use crate::{Error, Result};
 use argon2::Config;
 use rand::Rng;
 
+pub type Hash = fn(password: &str) -> Result<String>;
+
 pub fn hash(password: &str) -> Result<String> {
     let salt: [u8; 32] = rand::thread_rng().gen();
     let config = Config::default();
 
     argon2::hash_encoded(password.as_bytes(), &salt, &config).map_err(|_e| Error::internal_error())
 }
+
+pub type Verify = fn(hash: &str, attempted_password: &str) -> Result<()>;
 
 pub fn verify(hash: &str, attempted_password: &str) -> Result<()> {
     let verified = argon2::verify_encoded(hash, attempted_password.as_bytes())

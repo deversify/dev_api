14cf927b707636467dfbf6aa8d5a9ce7b075d758
Marcus Rådell <marcus@radell.net>
2022-08-09 15:40:29 +0200

    expose separate endpoints for mysql and postgres migrations

--- src/domains/migrations/repo.rs
+++ src/domains/migrations/repo.rs
@@ -1,18 +1,33 @@
-use sqlx::{MySql, Pool};
+use sqlx::{MySql, Pool, Postgres};
 
-use crate::db::migrate_mysql;
+use crate::db::{migrate_mysql, migrate_postgres};
 
 #[derive(Clone, Debug)]
-pub struct RepoImpl {
+pub struct RepoMySqlImpl {
     pub pool: Pool<MySql>,
 }
 
-impl RepoImpl {
+impl RepoMySqlImpl {
     pub fn new(pool: Pool<MySql>) -> Self {
         Self { pool }
     }
 
     pub async fn run(&self) -> crate::Result<()> {
         migrate_mysql(&self.pool).await
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct RepoPostgresImpl {
+    pub pool: Pool<Postgres>,
+}
+
+impl RepoPostgresImpl {
+    pub fn new(pool: Pool<Postgres>) -> Self {
+        Self { pool }
+    }
+
+    pub async fn run(&self) -> crate::Result<()> {
+        migrate_postgres(&self.pool).await
     }
 }
--- src/domains/migrations/run.rs
+++
@@ -1,8 +0,0 @@
-use super::RepoImpl;
-use actix_web::{web, HttpResponse, Responder};
-
-pub async fn controller(repo: web::Data<RepoImpl>) -> crate::Result<impl Responder> {
-    repo.run().await?;
-
-    Ok(HttpResponse::Ok())
-}

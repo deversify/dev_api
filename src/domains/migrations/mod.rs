1af921ab274e06c474a1b2a9b279489d07ed9215
Marcus Rådell <marcus@radell.net>
2022-08-09 13:27:39 +0200

    add call to repo.run which runs migrations

--- src/domains/migrations/mod.rs
+++ src/domains/migrations/mod.rs
@@ -1,6 +1,9 @@
 use actix_web::web;
 
+mod repo;
 mod run;
+
+pub use repo::*;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/migrations").route("/run", web::get().to(run::controller)));

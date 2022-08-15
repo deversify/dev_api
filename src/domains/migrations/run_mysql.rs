54239dbb323b15f0b07db107b1da3812c09d6265
Marcus Rådell <marcus@radell.net>
2022-08-15 13:35:44 +0200

    add tracing instrumentation

--- src/domains/migrations/run_mysql.rs
+++ src/domains/migrations/run_mysql.rs
@@ -1,6 +1,7 @@
 use super::RepoMySqlImpl;
 use actix_web::{web, HttpResponse, Responder};
 
+#[tracing::instrument(name = "mysql_migrations:run:controller", skip(repo))]
 pub async fn controller(repo: web::Data<RepoMySqlImpl>) -> crate::Result<impl Responder> {
     repo.run().await?;
 

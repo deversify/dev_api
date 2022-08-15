2247f9f8cc4f1c9c8e53daa886c6b57139d13747
Marcus Rådell <marcus@radell.net>
2022-08-15 13:35:44 +0200

    add tracing instrumentation

--- src/domains/migrations/run.rs
+++ src/domains/migrations/run.rs
@@ -1,6 +1,7 @@
 use super::RepoPostgresImpl;
 use actix_web::{web, HttpResponse, Responder};
 
+#[tracing::instrument(name = "migrations:run:controller", skip(repo))]
 pub async fn controller(repo: web::Data<RepoPostgresImpl>) -> crate::Result<impl Responder> {
     repo.run().await?;
 

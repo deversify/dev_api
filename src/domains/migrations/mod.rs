f02dd0768ec91f2788b2913c348c42f0c902e4c0
Marcus Rådell <marcus@radell.net>
2022-08-15 13:07:14 +0200

    change postgres migrations run to be the default alternative

--- src/domains/migrations/mod.rs
+++ src/domains/migrations/mod.rs
@@ -1,17 +1,15 @@
 use actix_web::web;
 
 mod repo;
+mod run;
 mod run_mysql;
-mod run_postgres;
 
 pub use repo::*;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/mysql_migrations").route("/run", web::get().to(run_mysql::controller)),
-    );
+    cfg.service(web::scope("/migrations").route("/run", web::get().to(run::controller)));
 
     cfg.service(
-        web::scope("/postgres_migrations").route("/run", web::get().to(run_postgres::controller)),
+        web::scope("/mysql_migrations").route("/run", web::get().to(run_mysql::controller)),
     );
 }
